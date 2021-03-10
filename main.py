520781dc703e6235f398462df12eff60b84df24a
Vaschenko Bogdan <bogdan.vaschenko@gmail.com>
2021-03-10 17:03:14 +0300

    2nd func

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from pprint import pprint
 
-def read_recipe(filepath):
+cook_book = {}
 
+def read_recipe(filepath):
     meal = []
     with open(filepath, 'r') as f:
-        dish_names = []
-        cook_book = {}
         for line in f:
-            line = line.strip('\n')
-            if line.find('|') < 0 and not line.isspace() and not line.isdigit():
-                dish_name = line.strip('\n')
-                print(dish_name)
-                dish_names.append(dish_name)
-                print(dish_names)
-                cook_book.fromkeys(dish_names, None)
+            line = line.strip()
+            if line.find('|') < 0 and not line.isspace() and not line.isdigit() and not line == '':
+                dish_name = line
+                cook_book.update({dish_name : None})
             elif line.find('|') > 0:
                 ingredient = line.split(' | ')
                 ingr = {'ingredient_name': ingredient[0], 'quantity': ingredient[1], 'measure': ingredient[2]}
                 meal.append(ingr)
-        cook_book.fromkeys(dish_names)
+                cook_book.update({dish_name : meal})
+            elif line == '':
+                meal = []
     pprint(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        print(cook_book[dish])
+        for ingr in cook_book[dish]:
+            if shop_list[ingr['ingredient_name']]:
+                shop_list[ingr['ingredient_name']]['quantity'] = shop_list[ingr['ingredient_name']]['quantity'] + ingr['quantity']
+            shop_list.update({ingr['ingredient_name'] : {'quantity': ingr['quantity'] * person_count, 'measure': ingr['measure']}})
+    pprint(shop_list)
+
 
 if __name__ == '__main__':
     read_recipe('recipes.txt')
+    get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
