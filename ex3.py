7d4ba0b90fa8caa5cb53817ec63091624dd13ca5
Vaschenko Bogdan <bogdan.vaschenko@gmail.com>
2021-03-11 19:11:52 +0300

    Ex3 done

--- ex3.py
+++ ex3.py
@@ -4,12 +4,20 @@ def sort_file(files):
     for file in files:
         with open(file, 'r', encoding='UTF-8') as f:
             for id,line in enumerate(f):
-                print(f"{id} {line}", end = '')
                 sum_lines.update({file : id+1})
 
-    a = tuple(sum_lines)
-    print(sum_lines)
-    print(a)
+    a = list(sum_lines.items())
+    a.sort(key=lambda p: p[1])
+    for pair in a:
+        with open('res.txt', 'a', encoding='UTF-8') as t:
+            t.write('\n' + pair[0] + '\n')
+            t.write(str(pair[1]) + '\n')
+        with open(pair[0], 'r', encoding='UTF-8') as e:
+            for line in e:
+                with open('res.txt', 'a', encoding='UTF-8') as r:
+                    r.write(line)
+
+
 
 
 file_list = ['1.txt', '2.txt', '3.txt']
